7c604e42b8ebefff108ef3a52a5de42d3530b27f
asd <modestolopeziii@gmail.com>
2014-06-02 12:54:17 -0700

    Update

--- news.php
+++ news.php
@@ -4,12 +4,11 @@
 	<link rel= "stylesheet" href= "css/normalize.css" />
 	<link href='http://fonts.googleapis.com/css?family=Lato:100,300' rel='stylesheet' type='text/css'>
 </head><body>
-
-
+<?php require('../wordpress-3.9/wordpress/wp-blog-header.php'); ?>
 <!-- HERO -->
 	<section class= "hero">
 		<header class= "menu-navigation">
-				<a href= "#" id= "mobile-icon-button">
+			<a href= "#" id= "mobile-icon-button">
 					<ul class= "mobile-menu-navigation" id= "mobile-icon-button">
 						<li>
 							<div class= "mobile-icon-container">
@@ -30,6 +29,8 @@
 				<li><a href= "#" class= "common-navigation"> Jobs |</a> </li>
 			</ul>
 			<ul class= "main-menu">
+				<li><a href= "index.html" class= "about-navigation common-navigation"> Home </a> </li>
+				
 				<li><a href= "about.html" class= "about-navigation common-navigation"> About </a> </li>
 				
 				<li><a href= "business-solution.html" class= "product-navigation common-navigation"> Product </a> </li>
@@ -40,7 +41,6 @@
 			</ul>
 
 		</header>
-		
 		<section class= "mobile-sidebar-container">
 					<ul>
 						<a href= "#about-section" class= "about-navigation common-navigation"><li> <figure class= "common-mobile-icon about-icon"></figure>About</li></a>
@@ -51,69 +51,60 @@
 						<a href= "news.html" class= "common-navigation"><li><figure class= "common-mobile-icon about-icon"></figure> Client</li></a>
 					</ul>
 			</section>
-	</section>
-
-<!--PRODUCT SECTION-->
-		<section class= "product-section" id= "product-section">
-			
-			<section class= "product-container">
-				<section class= "product-title editable"> <h1>Product and Business Solution</h1> </section>
-				
-				<header class= "product-navigation">
-					<ul>
-						<li><a href= "#" class= "editable">Payroll System</a>
-<!--							<ul class= "payroll-sub-menu-container list-down">
-								<li class= "payroll-sub-menu"><a href= "#"> Client Server </a></li>
-								<li class= "payroll-sub-menu"><a href= "#"> Online </a></li>
-								<li class= "payroll-sub-menu"><a href= "#"> Outsourcing </a></li>
-							</ul>
--->
-						</li>
-						<li><a href= "#" class= "editable"> Human Capital Management (HCM) </a></li>
-						<li><a href= "#" class= "editable">  Accounting System </a></li>
-						<li><a href= "#" class= "editable">  School/Education System </a></li>
-					</ul>
-				</header>
-				
-				<section class= "product-1">
-					<figure class= "product-image">
-						<section class= "product-description"></section>
-					</figure>
-					<section class= "product-title">  </section>
-				</section>
-
-				<section class= "product-1">
-					<figure class= "product-image"></figure>
-					<section class= "product-title">  </section>
-				</section>
-
-				<section class= "product-1">
-					<figure class= "product-image"></figure>
-					<section class= "product-title">  </section>
-				</section>
-
-				<section class= "product-1">
-					<figure class= "product-image"></figure>
-					<section class= "product-title">  </section>
-				</section>
-
-				<section class= "product-1">
-					<figure class= "product-image"></figure>
-					<section class= "product-title">  </section>
-				</section>
-
-				<section class= "product-1">
-					<figure class= "product-image"></figure>
-					<section class= "product-title">  </section>
-				</section>
-
-			<div>
-			<span class= "common-link"> <a href= "business-solution.html"> View all projects >> </a> </span>
-			</div>
-			</section>
+	<!--
+		<section class= "hero-container">
+		
+		<p class= "hero-description"> <mark class= "quote">"</mark>
+			We simplify <mark class= "highlight-underline">Software Solution</mark>.<mark class= "quote">"</mark>
+		</p>	
+			<section class= "hero-title"> - Lee System Technology Ventures </section>
+		
 		</section>
-		<!--end PRODUCT-->
+			<figure class= "icon-down"></figure>
+	<!--end HERO-->
+	</section><br/><br/><br/><br/><br/>
 
+
+
+	<!--NEWS-->
+			<section class= "news-section">
+				<section class= "news-container">
+				<section class= "news title"> <h1> News </h1> </section>
+				<nav class= "news-navigation-container">
+					<ul>
+						<li class= "active-menu"> Recent </li>
+						<li> Trending </li>
+					</ul>
+				</nav>
+<?php
+	global $post;
+	$args = array( 'posts_per_page' => 10 );
+	$myposts = get_posts( $args );
+	foreach( $myposts as $post ) :	setup_postdata($post);
+?>
+			<!--NEWS 1-->
+					<section class= "news-wrap">
+						<figure class= "news-image"> </figure>
+						<section class= "news-description">
+							<h3> <a class= "read-more-link" href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>  </h3>
+							<section class= "news-social-media">
+								<ul>
+									<li><strong>4/4/2014</strong></li>
+									<li>| 5 mins read</li>
+									<li><figure class= "fb-share-icon"></figure>	</li>
+									<li><figure class= "twitter-share-icon"></figure></li>
+								</ul>
+							</section>
+							<p> <a class= "read-more-link" href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_excerpt(); ?></a> 
+							</p>
+						</section>
+					</section>
+				<!--end NEWS 1-->
+<?php endforeach; ?>				
+				</section>
+
+			</section>
+		<!--end NEWS-->
 
 <!-- FOOTER -->
 	<section class= "footer-section">
@@ -124,4 +115,5 @@
 <!--end FOOTER-->
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <script src= "js/main.js"></script>
-</body></html>
+</body>
+</html>
